8d291228c1aef43cfbcf96e263a90c9276b2e22a
DESKTOP-FSRVJ63\gram <20kimkayeon@gmail.com>
2025-04-30 13:01:06 +0900

    feat: 자동 로그인 버튼 색상 반전, 화면 폰트 변경 #1

--- app/src/main/java/com/example/android/LoginActivity.kt
+++ app/src/main/java/com/example/android/LoginActivity.kt
@@ -9,6 +9,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
+    private var isChecked: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -21,6 +22,16 @@ class LoginActivity : AppCompatActivity() {
         binding.textView16.setOnClickListener {
             val intent = Intent(this, AgreefirstActivity::class.java)
             startActivity(intent)
+        }
+        binding.imageView12.setOnClickListener {
+            // 토글 상태 변경
+            isChecked = !isChecked
+            // 이미지 변경
+            if (isChecked) {
+                binding.imageView12.setImageResource(R.drawable.btn_checkbox_checked)
+            } else {
+                binding.imageView12.setImageResource(R.drawable.btn_checkbox)
+            }
         }
     }
 }
